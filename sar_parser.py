mbaldessari/sarstats

commit 6fce7b42bc2c025dc20aa59547726af7436379e9

Add --start and --end options to select time range

sar_parser.py
@@ -29,9 +29,11 @@ Hat Enterprise Linux versions 3 through 6 and from Fedora 20
 """
 
 import datetime
+import dateutil
 import os
 import numpy
 import re
+import sys
 
 import sar_metadata
 from sos_report import SosReport
@@ -65,7 +67,7 @@ def _average_line(line):
 def canonicalise_timestamp(date, ts):
     """sar files start with a date string (yyyy-mm-dd) and a 
     series of lines starting with the time. Given the initial
-    sar datetime date object as base and the time string columnt
+    sar datetime date object as base and the time string column
     return a full datetime object"""
 
     matches = re.search(TIMESTAMP_RE, ts)
@@ -101,7 +103,7 @@ class SarParser(object):
     # This dict holds the relationship graph->category
     _categories = {}
 
-    def __init__(self, fnames):
+    def __init__(self, fnames, starttime=None, endtime=None):
         """Constructor: takes a list of files to be parsed. The parsing
         itself is done in the .parse() method"""
         self._files = fnames
@@ -115,6 +117,14 @@ class SarParser(object):
         # If this one was set it means that we crossed the day during one SAR file
         self._olddate = None
         self._prev_timestamp = None
+        self.starttime = None
+        self.endtime = None
+        if starttime:
+            self.starttime = dateutil.parser.parse(starttime)
+
+        if endtime:
+            self.endtime = dateutil.parser.parse(endtime)
+
 
         # Current line number (for use in reporting parse errors)
         self._linecount = 0
@@ -278,6 +288,12 @@ class SarParser(object):
     def _record_data(self, headers, matches):
         """Record a parsed line of data"""
         timestamp = canonicalise_timestamp(self._date, matches.group(1))
+        # We skip recording values if the timestamp is not within the limits
+        # defined by the user
+        if self.starttime and timestamp < self.starttime:
+            return
+        if self.endtime and timestamp > self.endtime:
+            return
         if self._prev_timestamp and timestamp:
             # FIXME: This breaks if sar interval is bigger > 119 mins
             if self._prev_timestamp.hour == 23 and timestamp.hour == 0:

sarstats
@@ -94,6 +94,16 @@ if __name__ == '__main__':
                         at those times.  This is useful for correlation
                         work""")
 
+    parser.add_argument('--start', dest='starttime', default=None, action='append', help="""
+                        Sets a start time for the data to be evaluated.
+                        --start '2014-01-01 13:45:03'. Entries before the start
+                        time will be ignored.""")
+
+    parser.add_argument('--end', dest='endtime', default=None, action='append', help="""
+                        Sets an end time for the data to be evaluated.
+                        --end '2014-01-01 13:45:03'. Entries after the end
+                        time will be ignored.""")
+
     parser.add_argument('--list', dest='list_graphs', action='store_true', help="""
                         Only lists all the graphs and elements contained in the
                         specified sar files""")
@@ -159,7 +169,7 @@ if __name__ == '__main__':
         print("Error: No sar files passed as argument")
         sys.exit(-1)
 
-    sar_grapher = SarGrapher(args.sar_files)
+    sar_grapher = SarGrapher(args.sar_files, args.starttime[0], args.endtime[0])
     print()
 
     try:

sar_grapher.py
@@ -20,7 +20,7 @@ def ascii_date(d):
 
 
 class SarGrapher(object):
-    def __init__(self, filenames):
+    def __init__(self, filenames, starttime=None, endtime=None):
         """Initializes the class, creates a SarParser class
         given a list of files and also parsers the files"""
         # Temporary dir where images are stored (one per graph)
@@ -30,7 +30,7 @@ class SarGrapher(object):
         # matplotlib is simply inefficient in this area)
         self._tempdir = tempfile.mkdtemp(prefix='sargrapher')
 
-        self.sar_parser = SarParser(filenames)
+        self.sar_parser = SarParser(filenames, starttime, endtime)
         self.sar_parser.parse()
 
     def _graph_filename(self, graph, extension='.png'):

sar_stats.py
@@ -266,7 +266,7 @@ class SarStats(object):
         self.story.append(h)
 
     def graph(self, sar_files, skip_list, output_file='out.pdf', labels=None,
-              show_reboots=False, custom_graphs=''):
+            show_reboots=False, custom_graphs=''):
         """ Parse sar data and produce graphs of the parsed data. """
         sar_grapher = self.sar_grapher
         sar_parser = sar_grapher.sar_parser

commit fa8d3f0252babcb40cc628b8bbebd824508e8d47

Fix invocation without parameters
Commit 6fce7b42bc2c broke the invocation without --start and --end.
Fixes Issue #3

sar_parser.py
@@ -122,10 +122,10 @@ class SarParser(object):
         self.starttime = None
         self.endtime = None
         if starttime:
-            self.starttime = dateutil.parser.parse(starttime)
+            self.starttime = dateutil.parser.parse(starttime[0])
 
         if endtime:
-            self.endtime = dateutil.parser.parse(endtime)
+            self.endtime = dateutil.parser.parse(endtime[0])
 
         # Current line number (for use in reporting parse errors)
         self._linecount = 0

sarstats
@@ -169,7 +169,7 @@ if __name__ == '__main__':
         print("Error: No sar files passed as argument")
         sys.exit(-1)
 
-    sar_grapher = SarGrapher(args.sar_files, args.starttime[0], args.endtime[0])
+    sar_grapher = SarGrapher(args.sar_files, args.starttime, args.endtime)
     print()
 
     try: